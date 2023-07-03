960ef092e9c20b41caefab270929e7942b1d21ac
Kalai-vani-23 <kvanisen@gmail.com>
2023-07-03 20:20:31 +0530

    Commit as file changed

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from urllib.request import urlopen
-import re
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import random
 
-
+# Scrapping github movies.csv page
 response = requests.get(
     'https://gist.github.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea')
 soup = BeautifulSoup(response.content, "html.parser")
 web_html = soup.find(
     "table", attrs={"class": 'js-csv-data csv-data js-file-line-container'}).text
 # print(web_html)
+
+# Reading the table contents
 table = soup.table
 table_header = table.find_all('th')
 headers = []
@@ -25,25 +25,29 @@ for tr in table_rows:
     temp = []
     for data in td:
         if data.text:
-            temp.append(data.text.lower())
+            temp.append(data.text.upper())
     rows[i] = temp
     i += 1
 rows.pop(0)
-data_frame = pd.DataFrame(headers)
-data_frame = data_frame.T
-# print(data_frame)
+
+
 try:
+    # Writing data to output.csv using pandas
+    data_frame = pd.DataFrame(headers)
+    data_frame = data_frame.T
+    # print(data_frame)
     data_frame.to_csv('output.csv', index=False, header=False)
     data_frame = pd.DataFrame(rows)
     data_frame = data_frame.T
     data_frame.to_csv('output.csv', mode='a', index=False, header=False)
 
+    # Movie picker bot
     print("Enter any one genre from following:")
     input_sheet = pd.read_csv('output.csv')
     df = pd.DataFrame(input_sheet)
     genre = df['Genre'].unique()
     print(genre)
-    input_genre = input()
+    input_genre = str(input()).upper()
     if input_genre not in genre:
         print("Kindly enter anyone genre from the above list")
     else:
@@ -53,5 +57,7 @@ try:
             # print(df1['Film'].iloc[i])
             list1.append(df1['Film'].iloc[i])
         print(f"We can watch this {input_genre} movie:{random.choice(list1)}")
+
+# exception handling for file open
 except PermissionError:
     print("Kindly close the output.csv file")
